2b49211991a1ee1ebfcc29abac55c1f787e90f7f
Werner Dietl <wdietl@gmail.com>
2023-03-07 16:05:43 -0500

    Make javadoc more uniform

--- javacutil/src/main/java/org/checkerframework/javacutil/TypesUtils.java
+++ javacutil/src/main/java/org/checkerframework/javacutil/TypesUtils.java
@@ -298,9 +298,9 @@ public final class TypesUtils {
     /// Predicates
 
     /**
-     * Checks if the type represents a java.lang.Object declared type.
+     * Returns true iff the type represents a java.lang.Object declared type.
      *
-     * @param type the type
+     * @param type the type to check
      * @return true iff type represents java.lang.Object
      */
     public static boolean isObject(TypeMirror type) {
@@ -308,9 +308,9 @@ public final class TypesUtils {
     }
 
     /**
-     * Checks if the type represents the java.lang.Class declared type.
+     * Return true iff the type represents a java.lang.Class declared type.
      *
-     * @param type the type
+     * @param type the type to check
      * @return true iff type represents java.lang.Class
      */
     public static boolean isClass(TypeMirror type) {
@@ -318,9 +318,9 @@ public final class TypesUtils {
     }
 
     /**
-     * Checks if the type represents a java.lang.String declared type.
+     * Returns true iff the type represents a java.lang.String declared type.
      *
-     * @param type the type
+     * @param type the type to check
      * @return true iff type represents java.lang.String
      */
     public static boolean isString(TypeMirror type) {
@@ -328,9 +328,10 @@ public final class TypesUtils {
     }
 
     /**
-     * Returns true if the type is either boolean (primitive type) or java.lang.Boolean.
+     * Returns true iff the type represents either boolean (primitive type) or a java.lang.Boolean
+     * declared type.
      *
-     * @param type the type to test
+     * @param type the type to check
      * @return true iff type represents a boolean type
      */
     public static boolean isBooleanType(TypeMirror type) {
@@ -338,10 +339,10 @@ public final class TypesUtils {
     }
 
     /**
-     * Checks if the type represents a character type, i.e., it is either char (primitive type) or
-     * java.lang.Character.
+     * Returns true iff the type represents either char (primitive type) or a java.lang.Character
+     * declared type.
      *
-     * @param type the type to test
+     * @param type the type to check
      * @return true iff type represents a character type
      */
     public static boolean isCharType(TypeMirror type) {
@@ -349,9 +350,9 @@ public final class TypesUtils {
     }
 
     /**
-     * Check if the type represents a declared type of the given qualified name.
+     * Returns true iff the type represents a declared type of the given qualified name.
      *
-     * @param type the type
+     * @param type the type to check
      * @return type iff type represents a declared type of the qualified name
      */
     public static boolean isDeclaredOfName(TypeMirror type, CharSequence qualifiedName) {
@@ -360,7 +361,7 @@ public final class TypesUtils {
     }
 
     /**
-     * Check if the {@code type} represents a boxed primitive type.
+     * Returns true iff the {@code type} represents a boxed primitive type.
      *
      * @param type the type to check
      * @return true iff type represents a boxed primitive type
@@ -370,10 +371,13 @@ public final class TypesUtils {
     }
 
     /**
-     * Return true if this is an immutable type in the JDK.
+     * Returns true iff this is an immutable type in the JDK.
      *
      * <p>This does not use immutability annotations and always returns false for user-defined
      * classes.
+     *
+     * @param type the type to check
+     * @return true iff this is an immutable type in the JDK
      */
     public static boolean isImmutableTypeInJdk(TypeMirror type) {
         return isPrimitive(type)
@@ -382,9 +386,10 @@ public final class TypesUtils {
     }
 
     /**
-     * Returns true if type represents a Throwable type (e.g. Exception, Error).
+     * Returns true iff type represents a Throwable type (e.g. Exception, Error).
      *
-     * @return true if type represents a Throwable type (e.g. Exception, Error)
+     * @param type the type to check
+     * @return true iff type represents a Throwable type (e.g. Exception, Error)
      */
     public static boolean isThrowable(TypeMirror type) {
         while (type != null && type.getKind() == TypeKind.DECLARED) {
@@ -402,6 +407,7 @@ public final class TypesUtils {
     /**
      * Returns true iff the argument is an anonymous type.
      *
+     * @param type the type to check
      * @return whether the argument is an anonymous type
      */
     public static boolean isAnonymous(TypeMirror type) {
@@ -413,7 +419,7 @@ public final class TypesUtils {
     /**
      * Returns true iff the argument is a primitive type.
      *
-     * @param type a type
+     * @param type the type to check
      * @return whether the argument is a primitive type
      */
     public static boolean isPrimitive(TypeMirror type) {
