6376cb64ed59d9f3319cad206fa4de619b92f823
Werner Dietl <wdietl@gmail.com>
2023-04-23 12:21:17 -0400

    Use a fork of Daikon and use a fixed commit ID (#461)

--- checker/bin-devel/test-daikon.sh
+++ checker/bin-devel/test-daikon.sh
@@ -12,8 +12,10 @@ source "$SCRIPTDIR"/build.sh
 
 
 # daikon-typecheck: 15 minutes
-"$SCRIPTDIR/.plume-scripts/git-clone-related" codespecs daikon
+"$SCRIPTDIR/.plume-scripts/git-clone-related" eisop-codespecs daikon -q --single-branch --depth 50
 cd ../daikon
+# Use a known-working commit ID. Update this in a separate PR to confirm all tests pass.
+git checkout 3848b2f9459a5012a771ed2213f18336f1509866
 git log | head -n 5
 make compile
 if [ "$TRAVIS" = "true" ] ; then
