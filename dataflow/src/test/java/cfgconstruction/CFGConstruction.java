fa0f3db173fd1f58da8409e03e477a6ccb08f653
Werner Dietl <wdietl@gmail.com>
2023-11-22 23:23:14 -0500

    Formatting tweaks

--- dataflow/src/test/java/cfgconstruction/CFGConstruction.java
+++ dataflow/src/test/java/cfgconstruction/CFGConstruction.java
@@ -6,7 +6,6 @@ import org.checkerframework.dataflow.cfg.visualize.CFGVisualizeLauncher;
 public class CFGConstruction {
 
     public static void main(String[] args) {
-
         String inputFile = "Test.java";
         String clazz = "Test";
         String method = "manyNestedTryFinallyBlocks";
