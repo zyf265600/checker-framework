66ffae42b5623baa6097d31cc82c18115190cb25
Werner Dietl <wdietl@gmail.com>
2023-11-22 23:13:54 -0500

    Build JSpecify, not AFU, use right org, cleanups

--- checker/bin-devel/build.sh
+++ checker/bin-devel/build.sh
@@ -8,10 +8,8 @@ set -e
 
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source "$SCRIPTDIR"/clone-related.sh
-# Download dependencies, trying a second time if there is a failure.
-(TERM=dumb timeout 300 ./gradlew --write-verification-metadata sha256 help --dry-run || \
-     (sleep 1m && ./gradlew --write-verification-metadata sha256 help --dry-run))
-echo "running \"./gradlew assemble\" for checker-framework"
-./gradlew assemble --console=plain -Dorg.gradle.internal.http.socketTimeout=60000 -Dorg.gradle.internal.http.connectionTimeout=60000
+
+echo "running \"./gradlew assembleForJavac\" for checker-framework"
+./gradlew assembleForJavac --console=plain -Dorg.gradle.internal.http.socketTimeout=60000 -Dorg.gradle.internal.http.connectionTimeout=60000
 
 echo Exiting checker/bin-devel/build.sh in "$(pwd)"
