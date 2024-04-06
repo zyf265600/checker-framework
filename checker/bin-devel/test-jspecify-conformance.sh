43dcafd65db98173a02ee3e28193e143b0033ebc
Rohan Shetty <35475826+rohan-shettyy@users.noreply.github.com>
2024-04-05 20:09:56 -0400

    Run JSpecify conformance tests with the Nullness Checker (#709)

---
+++ checker/bin-devel/test-jspecify-conformance.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+set -o verbose
+set -o xtrace
+export SHELLOPTS
+echo "SHELLOPTS=${SHELLOPTS}"
+
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# shellcheck disable=SC1090 # In newer shellcheck than 0.6.0, pass: "-P SCRIPTDIR" (literally)
+export ORG_GRADLE_PROJECT_useJdk17Compiler=true
+source "$SCRIPTDIR"/clone-related.sh
+
+PLUME_SCRIPTS="$SCRIPTDIR/.plume-scripts"
+"$PLUME_SCRIPTS/git-clone-related" eisop jspecify-conformance
+cd ../jspecify-conformance
+./gradlew test --console=plain -PcfLocal
