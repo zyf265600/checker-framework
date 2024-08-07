61d8e8ea118b4ce9ee99dc7c6acaeb7b529b6299
Chris Povirk <cpovirk@google.com>
2024-08-07 12:09:59 -0400

    Fix build for JDK 22+ (#823)
    Co-authored-by: Werner Dietl <wdietl@gmail.com>

--- checker/src/main/java/org/checkerframework/checker/optional/OptionalVisitor.java
+++ checker/src/main/java/org/checkerframework/checker/optional/OptionalVisitor.java
@@ -510,7 +510,7 @@ public class OptionalVisitor
      * {@code x = Optional.of(Optional.of("baz"));}. However, the type of the right-hand side is
      * {@code Optional<? extends Object>}, not {@code Optional<Optional<String>>}. Therefore, to
      * fully check for improper types, it is necessary to examine, in the type checker, the argument
-     * to construction of an Optional. Method {@link handleNestedOptionalCreation} does so.
+     * to construction of an Optional. Method {@link #handleNestedOptionalCreation} does so.
      */
     private final class OptionalTypeValidator extends BaseTypeValidator {
 
