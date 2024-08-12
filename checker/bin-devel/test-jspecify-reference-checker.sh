d30ebee6d096acfbc03bf6eec8672770d046cd77
Werner Dietl <wdietl@gmail.com>
2024-08-11 21:34:58 -0400

    Check that building the JSpecify Reference Checker works (#821)

---
+++ checker/bin-devel/test-jspecify-reference-checker.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+set -o verbose
+set -o xtrace
+export SHELLOPTS
+echo "SHELLOPTS=${SHELLOPTS}"
+
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# shellcheck disable=SC1090 # In newer shellcheck than 0.6.0, pass: "-P SCRIPTDIR" (literally)
+export ORG_GRADLE_PROJECT_useJdk17Compiler=true
+source "$SCRIPTDIR"/clone-related.sh
+./gradlew assembleForJavac --console=plain -Dorg.gradle.internal.http.socketTimeout=60000 -Dorg.gradle.internal.http.connectionTimeout=60000
+
+GIT_SCRIPTS="$SCRIPTDIR/.git-scripts"
+# TODO: remove uses of `main-eisop` once that becomes `main`.
+"$GIT_SCRIPTS/git-clone-related" jspecify jspecify-reference-checker -b main-eisop
+
+# Delete the eisop/jdk that was already cloned...
+rm -r ../jdk
+# instead clone the jspecify/jdk.
+"$GIT_SCRIPTS/git-clone-related" jspecify jdk
+
+cd ../jspecify-reference-checker
+git switch main-eisop
+./gradlew build conformanceTests demoTest --console=plain --include-build "$CHECKERFRAMEWORK"
