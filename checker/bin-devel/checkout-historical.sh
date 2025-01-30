ffc312eafa9944319d37bfb362ac8d25dbef7841
Werner Dietl <wdietl@gmail.com>
2025-01-30 17:43:27 -0500

    Use EISOP scripts

--- checker/bin-devel/checkout-historical.sh
+++ checker/bin-devel/checkout-historical.sh
@@ -89,7 +89,7 @@ git checkout -B __merge_eval__
 echo "git-scripts"
 GIT_SCRIPTS="checker/bin-devel/.git-scripts"
 if [ ! -d "$GIT_SCRIPTS" ] ; then
-  git clone -q https://github.com/plume-lib/git-scripts.git "${GIT_SCRIPTS}"
+  git clone -q https://github.com/eisop-plume-lib/git-scripts.git "${GIT_SCRIPTS}"
 fi
 COMMIT="$(cd "${GIT_SCRIPTS}" && git rev-list -n 1 --first-parent --before="${commit_date}" master)"
 if [ -n "${COMMIT}" ] ; then
