1df4bc1d5666fd78d564bb141c558634a7cacf5d
Werner Dietl <wdietl@gmail.com>
2025-01-31 21:02:09 -0500

    Fix code comment formatting

--- framework/src/main/java/org/checkerframework/framework/stub/RemoveAnnotationsForInference.java
+++ framework/src/main/java/org/checkerframework/framework/stub/RemoveAnnotationsForInference.java
@@ -106,10 +106,8 @@ public class RemoveAnnotationsForInference {
      */
     public static void main(String[] args) {
         // TODO: using plume-lib's "Options" project here would be better, but would add a
-        // dependency to
-        // the whole Checker Framework, which is undesirable. Move this program elsewhere (e.g., to
-        // a
-        // plume-lib project)?
+        // dependency to the whole Checker Framework, which is undesirable. Move this program
+        // elsewhere (e.g., to a plume-lib project)?
         if (args[0].contentEquals("-keepFile")) {
             if (args.length < 2) {
                 System.err.println(
