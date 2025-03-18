2009bc3285ec40cf23046a47a02ffae5092e0fb0
Werner Dietl <wdietl@gmail.com>
2025-03-17 21:12:29 -0400

    Release: pass the old release version to script

--- docs/developer/release/release_build.py
+++ docs/developer/release/release_build.py
@@ -247,8 +247,8 @@ def build_checker_framework_release(
 
     # update versions
     ant_props = (
-        '-Dchecker=%s -Drelease.ver=%s -Dafu.version=%s -Dafu.properties=%s -Dafu.release.date="%s"'
-        % (checker_dir, version, version, afu_build_properties, afu_release_date)
+        '-Dchecker=%s -Dold.release.ver=%s -Drelease.ver=%s -Dafu.version=%s -Dafu.properties=%s -Dafu.release.date="%s"'
+        % (checker_dir, old_cf_version, version, version, afu_build_properties, afu_release_date)
     )
     # IMPORTANT: The release.xml in the directory where the Checker Framework is
     # being built is used. Not the release.xml in the directory you ran
