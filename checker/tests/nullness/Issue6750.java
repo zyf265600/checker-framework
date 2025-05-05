8e467877a307f4dc2b522265c1ea79c37f4da131
Werner Dietl <wdietl@gmail.com>
2025-05-04 20:03:28 -0400

    typetools/checker-framework 3.49.3 release (#1211)
    Co-authored-by: Suzanne Millstein <smillst@cs.washington.edu>
    Co-authored-by: Michael Ernst <mernst@cs.washington.edu>
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: Calvin Loncaric <marvinx03@gmail.com>
    Co-authored-by: Sascha Kehrli <84683849+skehrli@users.noreply.github.com>
    Co-authored-by: Sascha Kehrli <skehrli@ethz.ch>

---
+++ checker/tests/nullness/Issue6750.java
@@ -0,0 +1,49 @@
+package open.falsepos;
+
+import org.checkerframework.checker.nullness.qual.KeyFor;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+// @below-java17-jdk-skip-test
+public record Issue6750(String type) {
+
+    void needKeyFor(@KeyFor("#2") String s, Map<String, String> map) {
+        throw new RuntimeException();
+    }
+
+    @KeyFor("#1") String returnKeyFor(Map<String, String> map) {
+        throw new RuntimeException();
+    }
+
+    Map<String, String> getMap(Function<String, String> s) {
+        throw new RuntimeException();
+    }
+
+    void use() {
+        // :: error: (argument.type.incompatible)
+        needKeyFor("", getMap(String::toString));
+        // :: error: (expression.unparsable.type.invalid) :: error: (assignment.type.incompatible)
+        @KeyFor("getMap(String::toString)") String s = returnKeyFor(new HashMap<>(getMap(String::toString)));
+    }
+
+    void method(List<Issue6750> externals) {
+        externals.stream().collect(Collectors.groupingBy(Issue6750::type)).entrySet().stream()
+                .forEach(
+                        values -> {
+                            // :: error: (assignment.type.incompatible)
+                            @KeyFor({}) String b = values.getKey();
+                        });
+    }
+
+    void test(List<Issue6750> externals) {
+        externals.stream().collect(Collectors.groupingBy(Issue6750::type)).entrySet().stream()
+                .forEach(
+                        values -> {
+                            throw new RuntimeException("");
+                        });
+    }
+}
